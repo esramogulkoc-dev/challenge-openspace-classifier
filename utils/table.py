980bf72c60c2300c881fcce31fb000523a34e523
Esra <esramogulkoc@gmail.com>
2025-10-27 11:49:26 +0100

    table.py

--- utils/table.py
+++ utils/table.py
@@ -1,1 +1,26 @@
+class Table:
+    
+    def __init__(self,capacity): 
+       self.capacity = capacity
+       self.seats = [Seat() for _ in range(capacity)] 
+       
+    def has_free_spot(self): 
+        for seat in self.seats:
+            if seat.free:  
+                return True
+        return False 
 
+
+    def assign_seat(self,name):   
+        for seat in self.seats:
+            if seat.free: 
+                seat.set_occupant(name) 
+                return(f"{name} is assigned to this table") 
+        return ("No seats left") 
+    
+    def left_capacity(self): 
+            left_capacity = 0
+            for seat in self.seats:
+                if seat.free:
+                    left_capacity +=1
+            return left_capacity
